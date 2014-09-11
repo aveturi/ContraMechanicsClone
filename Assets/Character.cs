bad848af3fd6de4ca22b8439a111d7f98764f9f4
Kaustubh Srivastava <kos.srivastava@gmail.com>
2014-09-11 15:39:51 -0400

    Added a cube moving in all directions

---
+++ Assets/Character.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+	public float 		speed = 6f;
+	public float        leftEdge = -10f;
+	public float        rightEdge = 10f;
+	public float        topEdge = 10f;
+	public float        bottomEdge = -10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 pos = transform.position;
+
+		if (Input.GetKey ("up")) {
+			pos.y += speed * Time.deltaTime;
+		}
+
+		if (Input.GetKey ("down")) {
+			pos.y -= speed * Time.deltaTime;
+		}
+
+		if (Input.GetKey ("right")) {
+			pos.x += speed * Time.deltaTime;
+		}
+
+		if (Input.GetKey ("left")) {
+			pos.x -= speed * Time.deltaTime;
+		}
+
+		if ( pos.x > leftEdge && pos.x < rightEdge && pos.y > bottomEdge && pos.y < topEdge) {
+			transform.position = pos;
+		}
+
+	}
+}
