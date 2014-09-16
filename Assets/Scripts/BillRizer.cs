092f01c4e8f0b0c0ef76c3b3fd193916f29e7b7f
Kaustubh Srivastava <kos.srivastava@gmail.com>
2014-09-16 14:29:41 -0400

    Added left boundary

--- Assets/Scripts/BillRizer.cs
+++ Assets/Scripts/BillRizer.cs
@@ -7,17 +7,18 @@ public class BillRizer : MonoBehaviour {
 	public float        rightEdge = 10f;
 	public float        topEdge = 10f;
 	public float        bottomEdge = -10f;
-
-	public Vector2		gravity = new Vector2(0f,-9f); //TODO:tune this.
+	
+	public float 		leftBoundary = 1.2f;
+	public Vector2		gravity = new Vector2(0f,-9f);
 	public Vector2		vel = Vector2.zero;
 
-	public float xSpeed = 0.09f;
-	public float jumpVal = 7f;
+	public float 		xSpeed = 0.09f;
+	public float 		jumpVal = 7f;
 
 
-	public bool onFloor = false;
-	public bool crouched = false;
-	// Use this for initialization
+	public bool 		onFloor = false;
+	public bool 		crouched = false;
+	
 	void Start () {
 	
 	}
@@ -43,7 +44,7 @@ public class BillRizer : MonoBehaviour {
 
 		// X == A
 		if (Input.GetKeyDown (KeyCode.X)) {
-			Debug.Log ("X!");
+			//Debug.Log ("X!");
 			if (onFloor) {
 				if (crouched) {
 					Debug.Log("Fall through");
@@ -61,12 +62,12 @@ public class BillRizer : MonoBehaviour {
 
 		// Z == B
 		if (Input.GetKey (KeyCode.Z)) {
-			Debug.Log("Z!");
+			//Debug.Log("Z!");
 			
 		}
 
 		if (Input.GetKeyDown (KeyCode.DownArrow)) {
-			Debug.Log("DOWN!");
+			//Debug.Log("DOWN!");
 			crouched = true;
 
 		}
@@ -78,15 +79,20 @@ public class BillRizer : MonoBehaviour {
 		}
 
 		if (Input.GetKey(KeyCode.LeftArrow)){
-			Debug.Log("LEFT!");
+			//Debug.Log("LEFT!");
 			Vector2 pos = transform.position;
 			pos.x -= xSpeed;
-			transform.position = pos;
-			
+
+			var vertExtent = Camera.main.camera.orthographicSize;   
+			var horzExtent = vertExtent * Screen.width / Screen.height;
+				
+			if (pos.x >= (Camera.main.camera.transform.position.x - horzExtent + leftBoundary)) {
+				transform.position = pos;
+			}
 		}
 
 		if (Input.GetKey(KeyCode.RightArrow)){
-			Debug.Log("RIGHT!");
+			//Debug.Log("RIGHT!");
 			Vector2 pos = transform.position;
 			pos.x += xSpeed;
 			transform.position = pos;
@@ -94,7 +100,7 @@ public class BillRizer : MonoBehaviour {
 		}
 
 		if (Input.GetKey (KeyCode.UpArrow)) {
-			Debug.Log("UP!");
+			//Debug.Log("UP!");
 		}
 
 
