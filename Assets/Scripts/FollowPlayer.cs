ca2ebd15f3d57bf67b92c868cceacf1a3451ccf0
Kaustubh Srivastava <kos.srivastava@gmail.com>
2014-09-15 22:22:54 -0400

    Camera follows player

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public GameObject		player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = player.transform.position;
+		pos.z = -10.0F;
+		transform.position = pos;
+	}
+}
