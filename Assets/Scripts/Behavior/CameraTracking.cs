36f8b412163fc45a49bcc8231b3282d78aa72d04
Anuraag Veturi <anuraagv@umich.edu>
2014-09-17 18:31:05 -0400

    added CamoSniper

--- Assets/Scripts/Behavior/CameraTracking.cs
+++ Assets/Scripts/Behavior/CameraTracking.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class CameraTracking : MonoBehaviour {
 
-	public GameObject		player;
+	public GameObject	player;
 
 	// Use this for initialization
 	void Start () {
