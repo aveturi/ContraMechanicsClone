2f432661a965579ed0f91a52b9f6a3771bc32a17
Kaustubh Srivastava <kos.srivastava@gmail.com>
2014-09-25 20:54:10 -0400

    changed magic nums

--- Assets/Scripts/Behavior/Lava.cs
+++ Assets/Scripts/Behavior/Lava.cs
@@ -4,21 +4,35 @@ using System.Collections;
 public class Lava : Boundary {
 
 	private float 	lavaSpeed = 1.2f;
+	private bool	canMove;
+	public float 	waitTime = 1f;
 
 	void Start() {
-
+		canMove = false;
+		Invoke ("CanMove", waitTime);
 	}
 
 	void Update() {
-		var pos = transform.position;
-		pos.y += lavaSpeed * Time.deltaTime;
-		transform.position = pos;
+		if (canMove) {
+			var pos = transform.position;
+			pos.y += lavaSpeed * Time.deltaTime;
+			transform.position = pos;
+		}
+	}
+
+	private void CanMove() {
+		canMove = true;
+	}
+
+	private void RollBackABit() {
+		waitTime = 1f;
+		canMove = false;
+		Invoke ("CanMove", waitTime);
 	}
 
 	protected void OnTriggerEnter2D (Collider2D other) {
 		if (other.tag == "BillRizer") {
-			//LavaTracking script = (LavaTracking) Camera.main.GetComponent(typeof(LavaTracking));
-			//script.ResetCamera();
+			RollBackABit();
 		}
 
 		OnTriggerExit2D (other);
