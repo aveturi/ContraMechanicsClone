d63db46ed5bf71f7d379efcdb95672e55f3d4867
Anuraag Veturi <anuraagv@umich.edu>
2014-09-26 13:08:56 -0400

    more level design

--- Assets/Scripts/Behavior/Lava.cs
+++ Assets/Scripts/Behavior/Lava.cs
@@ -3,10 +3,10 @@ using System.Collections;
 
 public class Lava : Boundary {
 
-	private float 	lavaSpeed = 1.2f;
+	private float 	lavaSpeed = 1.3f;
 	private float 	startingYDelta = 1.5f;
 	private bool	canMove;
-	public float 	waitTime = 1f;
+	public float 	waitTime = 1.5f;
 
 	void Start() {
 		ResetPosition ();
