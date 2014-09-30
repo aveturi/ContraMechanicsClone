4c19931c8004ff80aed06025b56dfa35a0b6c92c
Kaustubh Srivastava <kos.srivastava@gmail.com>
2014-09-30 15:07:07 -0400

    More progress

--- Assets/Scripts/Behavior/VerticalCameraTracking.cs
+++ Assets/Scripts/Behavior/VerticalCameraTracking.cs
@@ -4,14 +4,24 @@ using System.Collections;
 public class VerticalCameraTracking : MonoBehaviour {
 	
 	private GameObject	player;
-	public Color color1;
-	public Color color2;
-	private float duration = 5f;
-	private float deltaTime = 0.0f;
+	public GameObject	marker;
+	public GameObject	boundaryMarker;
+	public float		screenWidth;
+	public float		topEdge;
+	public Color 		color1;
+	public Color 		color2;
+	private float 		duration = 5f;
+	private float 		deltaTime = 0.0f;
+	public float		cameraSpeed = 5f;
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("BillRizer");
 		deltaTime = 0.0f;
+		screenWidth = (this.camera.orthographicSize * 2f * this.camera.aspect);
+		topEdge = boundaryMarker.transform.position.y;
+
+		GameObject boss = GameObject.Find ("Boss");
+		// TODO enable boss when bill hits marker
 	}
 	
 	// Update is called once per frame
@@ -36,11 +46,21 @@ public class VerticalCameraTracking : MonoBehaviour {
 		}
 
 		Vector3 pos = transform.position;
-		
-		pos.y = player.transform.position.y;
-		if (pos.y >= transform.position.y) {
-			transform.position = pos;
+
+		if (pos.y >= marker.transform.position.y) {
+			if (pos.y + screenWidth / 2 < topEdge) {
+				pos.y += cameraSpeed * Time.deltaTime;
+				transform.position = pos;
+			}
 		}
+		
+		else {
+			pos.y = player.transform.position.y;
+			if (pos.y >= transform.position.y) {
+				transform.position = pos;
+			}
+		}
+
 		var bottomLeft = this.camera.ViewportToWorldPoint (new Vector2(0, 0));
 		if (player.renderer.bounds.min.y <= bottomLeft.y) {
 			Bill bill = player.GetComponent<Bill>();
